9bc2bb3914502494d7ce83611118ea67d3a4a81d
Nisarg Jain <nisarg37jain@gmail.com>
2023-04-06 00:20:29 +0530

    firstcommit

--- LinkedList/linkedlist.py
+++ LinkedList/linkedlist.py
@@ -38,7 +38,7 @@ class linkedList:
             self.tail = self.tail.next
             self.len += 1
 
-
+/* nisarg */
 if __name__=='__main__':
     arr = [1,2,3,4,5,6,7]
     linkd = linkedList()
