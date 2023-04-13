8e7c34fe8b2d06b7666ce72c92a3882257a81417
Nisarg Jain <nisarg37jain@gmail.com>
2023-04-13 23:43:11 +0530

    sorting

---
+++ Sorting/selectionsort.py
@@ -0,0 +1,21 @@
+
+
+def selectionsort(A, i=None):      #T(i)
+    '''Sort A[:i + 1]'''
+    if i is None: i = len(A)- 1    #O(1)
+    if i>0: 
+        j = prefix_max(A, i)        
+        A[j], A[i] = A[i], A[j]    #O(1)
+        selectionsort(A, i-1)      #T(i - 1)
+
+def prefix_max(A, i):               #S(i)
+    if i>0:                         #O(1)
+        j = prefix_max(A, i-1)      #S(i-1)
+        if(A[j] > A[i]):            #O(1)
+            return j
+    return i
+
+if __name__=="__main__":
+    A = [3,15,657,8,2,34,7, 13 , 4, 7, 1]
+    selectionsort(A)
+    print(A)
